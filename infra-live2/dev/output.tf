4ca52f44317303bb32225b6b74c730f982e0125c
isreal <isrealurephu@gmail.com>
2025-06-12 19:57:50 +0100

    adjusted github workflow directory

--- infra-live2/dev/output.tf
+++ infra-live2/dev/output.tf
@@ -2,7 +2,7 @@ output "vpc_id" {
   value = module.vpc.public_subnet_ids
 }
 
-output "public_subnet_ids" {
-  value = module.vpc.public_subnet_ids
-}
+# output "public_subnet_ids" {
+#   value = module.vpc.public_subnet_ids
+# }
 
