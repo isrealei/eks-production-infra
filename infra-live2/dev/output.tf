5f404f76ff0420802b680b7c49ee35b8ba7fab94
isreal <isrealurephu@gmail.com>
2025-06-14 18:36:36 +0100

    added eks-blueprint module for kubernetes cluster

--- infra-live2/dev/output.tf
+++ infra-live2/dev/output.tf
@@ -24,9 +24,9 @@ output "cluster_version" {
   value = module.eks.cluster_version
 }
 
-# output "oidc_issuer" {
-#   value = module.eks.oidc_issuer
-# }
+output "oidc_issuer" {
+  value = module.eks.oidc_issuer
+}
 
 output "oidc_provider_arn" {
   value = module.eks.oidc_provider_arn
