e9fc13369db9b9b5aa1ef4e12a86ead12ad84cd2
isreal <isrealurephu@gmail.com>
2025-06-15 17:50:18 +0100

    updated

--- infra-live2/dev/version.tf
+++ infra-live2/dev/version.tf
@@ -2,6 +2,10 @@ terraform {
   backend "s3" {}
 }
 
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_name
+}
+
 provider "helm" {
   kubernetes {
     host                   = module.eks.cluster_endpoint
@@ -10,9 +14,11 @@ provider "helm" {
   }
 }
 
-
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_ca_certificate)
   token                  = data.aws_eks_cluster_auth.cluster.token
 }
+
+data "aws_region" "current" {}
+
