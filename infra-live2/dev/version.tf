1cd9fab1fc160c57258aaffd7c6660ae4f542e8c
isreal <isrealurephu@gmail.com>
2025-06-18 17:25:22 +0100

    fixed typos

--- infra-live2/dev/version.tf
+++ infra-live2/dev/version.tf
@@ -7,7 +7,7 @@ data "aws_eks_cluster_auth" "cluster" {
 }
 
 provider "helm" {
-  kubernetes {
+  kubernetes = {
     host                   = module.eks.cluster_endpoint
     cluster_ca_certificate = base64decode(module.eks.cluster_ca_certificate)
     token                  = data.aws_eks_cluster_auth.cluster.token
