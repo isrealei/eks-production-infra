2e9b0aca084af2b3e79b7454c14064438a5cbcb7
isreal <isrealurephu@gmail.com>
2025-08-02 18:04:10 +0100

    added files

--- infra-live2/variable.tf
+++ infra-live2/variable.tf
@@ -72,5 +72,19 @@ variable "principal_arn_name" {
   type        = string
   description = "Name of the principal ARN, used for identification in the EKS module"
   default     = "admin"
+  
+}
 
+variable "node_groups" {
+  type = map(object({
+    instance_types = list(string)
+    capacity_type  = string
+    scaling_config = object({
+      desired_size = number
+      max_size     = number
+      min_size     = number
+    })
+  }))
+  description = "Map of node groups with their configurations"
+  default     = {}
 }
