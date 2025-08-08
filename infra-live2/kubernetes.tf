588c9cd686589b1ec1df4b1cc77301a4aa6ed8b2
isreal <isrealurephu@gmail.com>
2025-08-08 15:13:54 +0100

    provision infra

--- infra-live2/kubernetes.tf
+++ infra-live2/kubernetes.tf
@@ -15,34 +15,34 @@ resource "kubernetes_secret" "argo-repo" {
 }
 
 
-# installing karpenter
-resource "helm_release" "karpenter" {
-  name             = "karpenter"
-  repository       = "oci://public.ecr.aws/karpenter/karpenter"
-  chart            = "karpenter"
-  version          = "1.6.0"
-  namespace        = "karpenter"
-  create_namespace = true
+# # installing karpenter
+# resource "helm_release" "karpenter" {
+#   name             = "karpenter"
+#   repository       = "oci://public.ecr.aws/karpenter/karpenter"
+#   chart            = "karpenter"
+#   version          = "1.6.0"
+#   namespace        = "karpenter"
+#   create_namespace = true
 
-  values = [
-    yamlencode({
-      settings = {
-        clusterName       = var.cluster_name
-        interruptionQueue = var.cluster_name
-      }
-      controller = {
-        resources = {
-          limits = {
-            cpu    = "1"
-            memory = "1Gi"
-          }
-          requests = {
-            cpu    = "1"
-            memory = "1Gi"
-          }
-        }
-      }
-    })
-  ]
-  wait = true
-}
+#   values = [
+#     yamlencode({
+#       settings = {
+#         clusterName       = var.cluster_name
+#         interruptionQueue = var.cluster_name
+#       }
+#       controller = {
+#         resources = {
+#           limits = {
+#             cpu    = "1"
+#             memory = "1Gi"
+#           }
+#           requests = {
+#             cpu    = "1"
+#             memory = "1Gi"
+#           }
+#         }
+#       }
+#     })
+#   ]
+#   wait = true
+# }
