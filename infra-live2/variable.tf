d334ffdd7c4ac952f1c233f12438489862b3df17
isreal <isrealurephu@gmail.com>
2025-08-09 07:08:22 +0100

    removed defaukt values

--- infra-live2/variable.tf
+++ infra-live2/variable.tf
@@ -101,12 +101,4 @@ variable "database_config" {
     db_engine_version = string
     project_name      = string
   })
-  default = {
-    db_name           = var.db_name
-    db_username       = var.db_username
-    db_password       = var.db_password
-    db_instance_class = var.db_instance_class
-    db_engine         = var.db_engine
-    db_engine_version = var.db_engine_version
-  }
 }
