f58a04efdc0539f9c6387869671dee2817a27a11
isreal <isrealurephu@gmail.com>
2025-08-09 20:29:26 +0100

    deployed

--- modules/database/main.tf
+++ modules/database/main.tf
@@ -34,3 +34,19 @@ resource "aws_db_instance" "default" {
   }
 }
 
+resource "aws_elasticache_cluster" "redis" {
+    cluster_id           = "${var.project_name}-redis"
+    engine               = "redis"
+    node_type            = "cache.t3.micro"
+    num_cache_nodes      = 1
+    parameter_group_name = "default.redis6.x"
+    subnet_group_name    = aws_db_subnet_group.default.name
+    security_group_ids   = [var.db_security_group]
+    az_mode = "cross-az"
+    
+    tags = {
+        Name        = "My Redis Cluster"
+        Environment = var.env
+        Project     = var.project_name
+    }
+}
