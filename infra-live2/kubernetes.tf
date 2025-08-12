d28e109a1ebf574350c6890b4b6622968f37311a
isreal <isrealurephu@gmail.com>
2025-08-12 18:15:44 +0100

    deployed

--- infra-live2/kubernetes.tf
+++ infra-live2/kubernetes.tf
@@ -36,3 +36,31 @@ resource "kubernetes_namespace" "evoting" {
     name = var.namespace
   }
 }
+
+
+
+
+data "aws_ecrpublic_authorization_token" "token" {}
+
+resource "helm_release" "karpenter" {
+  namespace           = "karpenter"
+  name                = "karpenter"
+  repository          = "oci://public.ecr.aws/karpenter"
+  repository_username = data.aws_ecrpublic_authorization_token.token.user_name
+  repository_password = data.aws_ecrpublic_authorization_token.token.password
+  chart               = "karpenter"
+  version             = "1.6.0"
+  wait                = false
+  create_namespace    = true
+
+  values = [
+    <<-EOT
+    serviceAccount:
+       name: ${module.karpenter.service_account}
+    settings:
+      clusterName: ${module.eks.cluster_name}
+      clusterEndpoint: ${module.eks.cluster_endpoint}
+      interruptionQueue: ${module.karpenter.queue_name}
+    EOT
+  ]
+}
